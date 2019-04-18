d278ad88f9cff4c7a3f669d1c01c674eea815e6e
Taras Moskovych <moskovych.taras@gmail.com>
2019-04-19 00:45:03 +0300

    fix navigation issues

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,10 @@ const routes: Routes = [
     path: '',
     redirectTo: 'login',
     pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: 'login'
   }
 ];
 
--- src/app/system/system.component.ts
+++ src/app/system/system.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-system',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SystemComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
-  }
+    const url = this.router.url.split('/');
 
+    if (url.length <= 2) {
+      this.router.navigate(['/system', 'bill']);
+    }
+  }
 }
