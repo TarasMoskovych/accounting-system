0b39325702be8c0c2231acea5676eac3be46accd
Taras Moskovych <moskovych.taras@gmail.com>
2019-05-12 20:30:26 +0300

    Prepare app for deploying

--- client/src/app/core/configs/api.configs.ts
+++ client/src/app/core/configs/api.configs.ts
@@ -1,6 +1,7 @@
 import { InjectionToken } from '@angular/core';
 
-const baseUrl = 'http://localhost:3000';
+// const baseUrl = 'http://localhost:3000';
+const baseUrl = 'https://accounting-system-ng.herokuapp.com';
 const accessKey = '63021dd5f337b15aefab6ee49a3b6b46';
 
 export const currencyApi = `http://data.fixer.io/api/latest?access_key=${accessKey}`;
